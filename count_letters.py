15ecd83dd7f5e6840bb154bc09c469b825316ef7
dawidciupka <python.snake@cyberia.com>
2022-09-28 19:57:33 +0200

    count let

---
+++ count_letters.py
@@ -0,0 +1,26 @@
+"""
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+What if the string is empty? Then the result should be empty object literal, {}.
+
+"""
+def count(string):
+    res={}
+    for chr in string:
+        if chr in res:
+            res[chr]+=1
+        else:
+            res[chr]=1
+    return res
+
+
+
+#other
+from collections import Counter
+
+def count(string):
+    return Counter(string)
+
+
+def count(string):
+    return {i: string.count(i) for i in string}
---
+++ int.py
@@ -0,0 +1,13 @@
+a = 14.1
+
+print(a)
+print(f'hash {hash(a)}')
+print(type(a))
+
+
+print('-------------')
+
+
+print(int(a))
+print(f'hash {hash(int(a))}')
+print(type(int(a)))
